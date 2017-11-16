60b238a96e9bc948eacac5df4b2498700dc91df1
MiskTF <mikkel.buhl@hotmail.com>
2017-11-16 22:42:17 +0900

    Fixed ScoreManager

--- Leap Motion/Assets/StudentGod/Scripts/ScoreManager.cs
+++ Leap Motion/Assets/StudentGod/Scripts/ScoreManager.cs
@@ -2,43 +2,39 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class ScoreManager : MonoBehaviour{
+public static class ScoreManager {
 
-    /*
-    public static Score gameScore;
-    public class Score() {
-        public static int score  = 0;
-        public static int hits   = 0;
-        public static int lates  = 0;
-        public static int misses = 0; 
-    }
+    private static class Score {
+        public static int score{ get; private set;}
+        public static int hits{ get; private set;}
+        public static int lates{ get; private set;}
+        public static int misses{ get; private set;} 
 
-    public static void initializeScore() {
-        gameScore = new Score();
-    }
+        public static void initializeScore() {
+            score = 0;
+            hits = 0;
+            lates = 0;
+            misses = 0;
+        }
 
-    public static void IncreaseScore(int amount) {
-        gameScore.score += amount;
-    }
+        public static void IncreaseScore(int amount) {
+            score += amount;
+        }
 
-    public static void DecreaseScore(int amount) {
-        gameScore.score -= amount;
-    }
+        public static void DecreaseScore(int amount) {
+            score -= amount;
+        }
 
-    public static void IncreaseHits() {
-        gameScore.hits++;
-    }
+        public static void IncreaseHits() {
+            hits++;
+        }
 
-    public static void IncreaseLates() {
-        gameScore.lates++;
-    }
+        public static void IncreaseLates() {
+            lates++;
+        }
 
-    public static void IncreaseMisses() {
-        gameScore.misses++;
+        public static void IncreaseMisses() {
+            misses++;
+        }
     }
-
-    public static Score GetScore() {
-        return gameScore;
-    }
-    */
 }
