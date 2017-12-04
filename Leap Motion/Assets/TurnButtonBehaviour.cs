04e216267b3f1a6605ffc314d0ea4bed45f14534
MiskTF <mikkel.buhl@hotmail.com>
2017-12-04 22:12:18 +0900

    Added panning button functionality. Added a few extra scenes for working in.

---
+++ Leap Motion/Assets/TurnButtonBehaviour.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Leap.Unity.Interaction;
+public class TurnButtonBehaviour : InteractionBehaviour {
+
+    public int isRight;
+    public Transform pivot;
+    public float turnSpeed;
+
+	// Use this for initialization
+	protected override void Start () {
+        base.Start ();  
+        OnHoverStay += turn;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    void turn() {
+        pivot.Rotate (new Vector3(0,turnSpeed*isRight*Time.deltaTime,0));
+    }
+
+
+}
